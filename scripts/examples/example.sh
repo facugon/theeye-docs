d1740e41f5effc189d3465470cf8b732de88905f
Facundo <facundo.siquot@gmail.com>
2017-12-13 10:21:30 -0300

    Update example.sh

--- scripts/examples/example.sh
+++ scripts/examples/example.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
-## For both Monitors and Tasks We need that your script ends by returning "normal" or "failure". 
+## For scripts used in monitors, it is required that the last line of the script print "normal" or "failure" string to stdout.
+## By default "failure" is assumed by the api e.g. if the final print is not a recognized state.
+
 STATE="normal"
-# run your command
+
+# your code goes here
+
 echo "Hello World"
-#change state if its need it. 
-#print state.
+
+# and finally print the state
+
 if [ $? -ne 0 ];then STATE="failure"; fi
 echo $STATE
-## You can even retrieve more information by sending json formatted string. 
+
+## It is also possible to print more information to the last line of the script that will be stored by the api.
+## you have to use json formatted string.
 ## 
-## I.E :
+## i.e. :
 ## echo '{"'$STATE'":"normal","data":{ "name1":"value1", "name2":"value2" }}'
