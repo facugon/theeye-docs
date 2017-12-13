656a208600144eebee3325ea7bb94afcb2684992
Facundo <facundo.siquot@gmail.com>
2017-12-13 10:23:55 -0300

    Update example.js

--- scripts/examples/example.js
+++ scripts/examples/example.js
@@ -1,5 +1,4 @@
-//For both Monitors and Tasks We need that your script ends by returning "normal" or "failure". 
-// WARNING : check your current path and name to node binary.
+#!/usr/bin/env node
 
 var state = 'normal';
 
@@ -10,8 +9,6 @@ if( false ) {
 
 console.log( state );
 
-//You can even retrieve more information by sending json formated string.
-//I.E
 //console.log( JSON.stringify({ state:state, data:{responseTime:responseTime} }) ); 
 
 process.exit;
